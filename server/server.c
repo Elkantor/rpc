8796b84b950f2782d7b3b7b89e8a2001a33fb567
Elkantor <vic03@live.fr>
2021-08-06 19:52:25 +0200

    Split includes headers dependencies into a dependencies file

--- server/server.c
+++ server/server.c
@@ -2,16 +2,7 @@
 ** listener.c -- a datagram sockets "server" demo
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include "dependencies.h"
 
 #define PORT "4950"    // the port users will be connecting to
 
